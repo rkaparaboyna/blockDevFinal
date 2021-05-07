789834c994d9bdea7e4ae98dd08089d3e55a2ae6
Rahul Kaparaboyna <rahul.kaparaboyna@gmail.com>
2021-05-06 19:57:43 -0700

    new

--- truffle-config.js
+++ truffle-config.js
@@ -4,8 +4,10 @@ module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
   networks: {
-    develop: {
-      port: 8545
+    development: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*"
     }
   },
   compilers: {
